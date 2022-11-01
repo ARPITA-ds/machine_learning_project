c0aac4b57f00a7ec952276889dcb6ce921c4b29a
ARPITA-ds <arpimaity1997@gmail.com>
2022-11-01 23:51:09 +0530

    Changes updated in pipeline

--- housing/pipeline/pipeline.py
+++ housing/pipeline/pipeline.py
@@ -1,25 +1,21 @@
-from cmath import e
-import imp
+
 from housing.config.configuration import Configuartion
 from housing.logger import logging
 from housing.exception import HousingException
 
-from housing.entity.artifact_entity import DataIngestionArtifact
+from housing.entity.artifact_entity import  DataIngestionArtifact
 from housing.entity.config_entity import DataIngestionConfig
 from housing.component.data_ingestion import DataIngestion
-
-import os,sys
-
+import os, sys
 class Pipeline:
+    
+    def __init__(self, config: Configuartion = Configuartion()) -> None:
+        try:
+            self.config = config
+        except Exception as e:
+            raise HousingException(e, sys) from e
 
-    def __init__(self,config: Configuartion=Configuartion) -> None:
-       try:
-         self.config = config
-
-       except Exception as e:
-        raise HousingException(e,sys) from e
-
-    def start_data_ingestion(self)->DataIngestionArtifact:
+    def start_data_ingestion(self) -> DataIngestionArtifact:
         try:
             data_ingestion = DataIngestion(data_ingestion_config=self.config.get_data_ingestion_config())
             
@@ -27,9 +23,11 @@ class Pipeline:
 
         except Exception as e:
             raise HousingException(e,sys) from e
+            
 
     def start_data_validation(self):
         pass
+            
 
     def start_data_transformation(self):
         pass
@@ -43,12 +41,9 @@ class Pipeline:
     def start_model_pusher(self):
         pass
 
-
-
     def run_pipeline(self):
         try:
-
             data_ingestion_artifact = self.start_data_ingestion()
-
         except Exception as e:
             raise HousingException(e,sys) from e
+        
