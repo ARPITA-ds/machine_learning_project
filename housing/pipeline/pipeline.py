659aa90160519d49440564af3e4c9bfd8b668bf5
ARPITA-ds <arpimaity1997@gmail.com>
2022-11-03 23:00:02 +0530

    updated

--- housing/pipeline/pipeline.py
+++ housing/pipeline/pipeline.py
@@ -1,15 +1,30 @@
-
+from collections import namedtuple
+from datetime import datetime
+import uuid
 from housing.config.configuration import Configuartion
 from housing.logger import logging
 from housing.exception import HousingException
+from threading import Thread
+from typing import List
 
+from multiprocessing import Process
 from housing.entity.artifact_entity import  DataIngestionArtifact
+from housing.entity.artifact_entity import DataValidationArtifact
 from housing.entity.config_entity import DataIngestionConfig
 from housing.component.data_ingestion import DataIngestion
+from housing.component.data_validation import DataValidation
 import os, sys
+
+import pandas as pd
+
+
+
+
+
 class Pipeline:
     
-    def __init__(self, config: Configuartion = Configuartion()) -> None:
+
+    def __init__(self, config: Configuartion = Configuartion() ) -> None:
         try:
             self.config = config
         except Exception as e:
@@ -18,32 +33,51 @@ class Pipeline:
     def start_data_ingestion(self) -> DataIngestionArtifact:
         try:
             data_ingestion = DataIngestion(data_ingestion_config=self.config.get_data_ingestion_config())
-            
             return data_ingestion.initiate_data_ingestion()
-
         except Exception as e:
-            raise HousingException(e,sys) from e
-            
+            raise HousingException(e, sys) from e
 
-    def start_data_validation(self):
-        pass
-            
+    def start_data_validation(self, data_ingestion_artifact: DataIngestionArtifact) \
+            -> DataValidationArtifact:
+        try:
+            data_validation = DataValidation(data_validation_config=self.config.get_data_validation_config(),
+                                             data_ingestion_artifact=data_ingestion_artifact
+                                             )
+            return data_validation.initiate_data_validation()
+        except Exception as e:
+            raise HousingException(e, sys) from e
 
     def start_data_transformation(self):
-        pass
+        try:
+           pass
+        except Exception as e:
+            raise HousingException(e, sys)
 
     def start_model_trainer(self):
-        pass
+        try:
+            pass
+        except Exception as e:
+            raise HousingException(e, sys) from e
 
     def start_model_evaluation(self):
-        pass
+        try:
+            pass
+        except Exception as e:
+            raise HousingException(e, sys) from e
 
     def start_model_pusher(self):
-        pass
+        try:
+            pass
+        except Exception as e:
+            raise HousingException(e, sys) from e
 
     def run_pipeline(self):
         try:
+           
             data_ingestion_artifact = self.start_data_ingestion()
+            data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
+            
         except Exception as e:
-            raise HousingException(e,sys) from e
-        
+            raise HousingException(e, sys) from e
+
+    
